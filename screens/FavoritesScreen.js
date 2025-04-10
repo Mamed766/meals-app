d70085e00929e23da1fbfea061ba6741e5c7bd45
Mamed766 <mehemmeddemirli57@gmail.com>
2025-04-10 13:39:56 +0400

    feat: favorite meals length added

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,27 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import { StyleSheet, Text } from "react-native";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
+import MealsList from "../components/MealsList/MealsList";
 
 function FavoritesScreen() {
-  return <Text>The Favorite Screen!</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  if (favoriteMeals.length > 0) {
+    return (
+      <View>
+        <Text>No favorite meals yet!</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const style = StyleSheet.create({});
